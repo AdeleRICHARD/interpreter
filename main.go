c7b20c4a3390ddc63ad531d9ec8ead5be368bb36
AdeleRICHARD <adrichard@figarocms.fr>
2024-02-16 11:04:01 +0100

    Finished 1.5

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/user"
 
+	"main.go/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	repl.Start(os.Stdin, os.Stdout)
 }
